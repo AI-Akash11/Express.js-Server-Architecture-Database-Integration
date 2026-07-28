a027c6ca852cda75c2df249d54578fa2148ca851
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-07-28 21:30:44 +0600

    Executing Pool and creating tables

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,42 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import {Pool} from "pg"
+import { Pool } from "pg";
 
 const app: Application = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(express.text());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_moqY92sQGgid@ep-super-math-aznbd5ns.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-})
+  connectionString:
+    "postgresql://neondb_owner:npg_moqY92sQGgid@ep-super-math-aznbd5ns.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+
+    console.log("Database connected succesfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -29,15 +52,15 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/", async (req: Request, res: Response) => {
   // console.log(req.body);
 
-  const {name, id, password} = req.body;
+  const { name, id, password } = req.body;
 
   res.status(201).json({
     message: "created",
     data: {
       id,
-      name
+      name,
     },
-  })
+  });
 });
 
 app.listen(port, () => {
